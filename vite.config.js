437fa45fd90b0c61deab6f0579ebd40ebea287bf
Paul <lolol2222@protonmail.com>
2023-04-03 18:04:55 +0200

    legal-info__header legal-info__nav legal-info__footer
    finish mobile header
    
    finish mobile and desktop nav
    
    include mobile and desktop footer

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ module.exports = defineConfig({
         homepage: resolve(__dirname, "src/index.html"),
         theCafe: resolve(__dirname, "src/das-cafe/index.html"),
         rusticaOrigin: resolve(__dirname, "src/rustica-origin/index.html"),
+        legalInfo: resolve(__dirname, "src/legal-info/index.html"),
       },
     },
   },
